eb81c548fcf00f68faa78ad2358f534b7f76dbcf
Frank Reding <frank.reding@gmail.com>
2014-06-23 17:34:18 +0100

    Ordering

--- Angular101/Demo1/js/scripts.js
+++ Angular101/Demo1/js/scripts.js
@@ -4,28 +4,42 @@ var nekkidApp = angular.module('nekkidApp', []);
 // Define our Main Controller
 nekkidApp.controller('MovieCtrl', function ($scope) {
 
+	function resetMovie() {
+		$scope.movie = { score: 0 };
+	}
+
+	resetMovie();
+
 	// Initialise our library with a couple of sample movies
 	$scope.movies = [
 		{
 			name: 'Naked Gun',
 			actors: 'Leslie Nielsen, Priscilla Presley',
 			score: 5
+		},
+		{
+			name: 'Xdsadsa',
+			actors: 'ssadsda',
+			score: 3
 		}
 	];
 
-	// Initialise the object that holds our add/edit movie
-	// so that we select the 'unrated' value in the score select
-	// by default
-	$scope.movie = { score: 0 };
+	// Sorting
+	$scope.predicate = 'name';
+
+	$scope.$watch('predicate', function (newVal, oldVal) {
+		console.log(newVal, oldVal);
+	});
 
 	// Save Handler
 	$scope.saveMovie = function () {
 		$scope.movies.push($scope.movie);
+		resetMovie();
 	};
 
 	// Edit Handler
 	$scope.editMovie = function (m) {
-		$scope.movie = m;
+		$scope.movie = angular.copy(m);
 	}
 
 });
