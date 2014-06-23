789fe8b9c2cde21e79e5afaa83ff7bc509a297b6
Frank Reding <frank.reding@gmail.com>
2014-06-23 11:46:55 +0100

    Basic functionality

--- Angular101/Demo1/js/scripts.js
+++ Angular101/Demo1/js/scripts.js
@@ -1,1 +1,31 @@
-/* Scripts */
+// Create App as Angular Module
+var nekkidApp = angular.module('nekkidApp', []);
+
+// Define our Main Controller
+nekkidApp.controller('MovieCtrl', function ($scope) {
+
+	// Initialise our library with a couple of sample movies
+	$scope.movies = [
+		{
+			name: 'Naked Gun',
+			actors: 'Leslie Nielsen, Priscilla Presley',
+			score: 5
+		}
+	];
+
+	// Initialise the object that holds our add/edit movie
+	// so that we select the 'unrated' value in the score select
+	// by default
+	$scope.movie = { score: 0 };
+
+	// Save Handler
+	$scope.saveMovie = function () {
+		$scope.movies.push($scope.movie);
+	};
+
+	// Edit Handler
+	$scope.editMovie = function (m) {
+		$scope.movie = m;
+	}
+
+});
