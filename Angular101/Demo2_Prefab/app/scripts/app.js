151f31a73d4f8406ea45a3b563d56237acdc50eb
Frank Reding <frank.reding@gmail.com>
2014-06-29 15:34:58 +0100

    More Slides, Demo2 Prefab

---
+++ Angular101/Demo2_Prefab/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name demo2App
+ * @description
+ * # demo2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('demo2App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
